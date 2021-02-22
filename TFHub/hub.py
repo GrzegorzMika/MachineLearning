f3efb5b25f28116973cf51197dd5a3f9c6990bc2
Grzegorz <g.w.mika@gmail.com>
2021-02-22 21:50:10 +0100

    TFHUb

---
+++ TFHub/hub.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+print("Version: ", tf.__version__)
+print("Eager mode: ", tf.executing_eagerly())
+print("Hub version: ", hub.__version__)
+print("GPU is", "available" if tf.config.list_physical_devices("GPU") else "NOT AVAILABLE")
+
+train_data, validation_data, test_data = tfds.load(
+    name="imdb_reviews",
+    split=('train[:60%]', 'train[60%:]', 'test'),
+    as_supervised=True)
+
+# https://www.tensorflow.org/tutorials/keras/text_classification_with_hub
+# https://www.tensorflow.org/datasets/api_docs/python/tfds
+# https://www.tensorflow.org/guide/data_performance
