a94c24120462fbdc5e66bf484eedc57386bf2732
GrzegorzMika <grzegorz.mika@relia-sol.pl>
2021-02-23 17:53:06 +0100

    keras tuner !

--- TFHub/hub.py
+++ TFHub/hub.py
@@ -1,9 +1,6 @@
-import os
-import numpy as np
-
 import tensorflow as tf
-import tensorflow_hub as hub
 import tensorflow_datasets as tfds
+import tensorflow_hub as hub
 
 print("Version: ", tf.__version__)
 print("Eager mode: ", tf.executing_eagerly())
@@ -15,6 +12,27 @@ train_data, validation_data, test_data = tfds.load(
     split=('train[:60%]', 'train[60%:]', 'test'),
     as_supervised=True)
 
-# https://www.tensorflow.org/tutorials/keras/text_classification_with_hub
-# https://www.tensorflow.org/datasets/api_docs/python/tfds
-# https://www.tensorflow.org/guide/data_performance
+embedding = "https://tfhub.dev/google/nnlm-en-dim50/2"
+hub_layer = hub.KerasLayer(embedding, input_shape=[],
+                           dtype=tf.string, trainable=True)
+
+model = tf.keras.Sequential()
+model.add(hub_layer)
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1))
+
+print(model.summary())
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_data.shuffle(10000).batch(512),
+                    epochs=10,
+                    validation_data=validation_data.batch(512),
+                    verbose=1)
+
+results = model.evaluate(test_data.batch(512), verbose=2)
+
+for name, value in zip(model.metrics_names, results):
+    print("%s: %.3f" % (name, value))
