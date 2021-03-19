bffc1f47e49c387884f19da38e33001021537b7a
GrzegorzMika <g.w.mika@gmail.com>
2021-03-19 10:31:29 +0000

    load data

--- AudioClassification/paths.sh
+++ AudioClassification/paths.sh
@@ -3,4 +3,5 @@
 # http://www.openslr.org/12/
 
 export FILE='train-clean-100.tar.gz'
-export LOCATION='/home/grzegorz/audio_data/'
+export LOCATION='/home/gmika/audio_data/'
+export UNTAR_LOCATION="${LOCATION}LibriSpeech/${FILE//.tar.gz}/"
