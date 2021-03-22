23e2e01181157034ca49272d3d80f2caa7ca08bc
GrzegorzMika <g.w.mika@gmail.com>
2021-03-22 16:06:35 +0000

    start preparing data

--- AudioClassification/metadata.py
+++ AudioClassification/metadata.py
@@ -3,9 +3,10 @@ import os
 import pickle
 
 location = os.environ['UNTAR_LOCATION']
+target_location = os.environ['LOCATION']
 file_name = os.environ['UNTAR_FILE']
 
-dirs = os.listdir(location)
+dirs = os.listdir(os.path.join(location, file_name))
 
 with open(os.path.join(location, "SPEAKERS.TXT"), "r") as rf:
     text = rf.read()
@@ -28,9 +29,9 @@ logging.info("Splitting available speakers into male and female speakers...")
 for d in dirs:
     gender = available_speakers[int(d)]
     if gender == 'M':
-        male_speaker.append(os.path.join(location, d))
+        male_speaker.append(os.path.join(location, file_name, d))
     else:
-        female_speaker.append(os.path.join(location, d))
+        female_speaker.append(os.path.join(location, file_name, d))
 
-with open(os.path.join(location, "speaker.pkl"), "wb") as wf:
+with open(os.path.join(target_location, "speaker.pkl"), "wb") as wf:
     pickle.dump([male_speaker, female_speaker], wf)
--- AudioClassification/paths.sh
+++ AudioClassification/paths.sh
@@ -5,4 +5,4 @@
 export FILE='train-clean-100.tar.gz'
 export UNTAR_FILE=${FILE//.tar.gz}
 export LOCATION='/home/gmika/audio_data/'
-export UNTAR_LOCATION="${LOCATION}LibriSpeech/UNTAR_FILE/"
+export UNTAR_LOCATION="${LOCATION}LibriSpeech/"
