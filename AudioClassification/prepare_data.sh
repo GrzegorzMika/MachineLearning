d93bc2c27a4d30b47d4d289118e9b4091eb4135e
Grzegorz <g.w.mika@gmail.com>
2021-03-23 23:48:12 +0100

    fix path

--- AudioClassification/prepare_data.sh
+++ AudioClassification/prepare_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source paths.sh
+mkdir "$LOCATION"/'dataset/'
 python metadata.py
 python data.py --length 60.0 --num_train_samples 80 --num_valid_samples 10 --num_test_samples 10 --simultaneous True
 python TFRecords.py
